2034f05d36415e95f6719c031f5f165b50771643
Joel Uzibor <josamuyi@yahoo.com>
2016-03-25 19:20:14 +0000

    changed gemfile and modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-    root 'pages#home'
+  
+    root 'welcome#index'
+    get 'home', to: 'pages#home'
     get 'about', to: 'pages#about'
+    
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
